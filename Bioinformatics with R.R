ee13ad1be81614eb5172d6cfcfd56de501633cc6
Pascal <Pascalonaho@gmail.com>
2024-05-11 05:37:11 +0100

    Updated my R file again

--- Bioinformatics with R.R
+++ Bioinformatics with R.R
@@ -95,8 +95,49 @@ array3d
 print(array3d[2, 1, 2])
 
 
+
 # To pull an entire row or column
 
 print(array3d[1, , 2])
 
 print(array3d[, 3, 2])
+
+
+# Lists and Data Frames
+
+HSAPA1A <- list(gene = "HSAPA1a", amino.acids = 641, nucleotides = 2400)
+
+print(HSAPA1A)
+
+HSAPA1A$amino.acids
+HSAPA1A$gene
+HSAPA1A$nucleotides
+
+gene <- c("HSAPA4", "HSAPA5", "HSPA8", "HSPA9", "HSPA1A", "HSPA1B")
+nucleotides <- c(54537, 6491, 4648, 21646, 2400, 2517)
+aminoAcids <- c(840, 654, 493, 679, 641, 641)
+
+HSPs <- data.frame(gene, nucleotides, aminoAcids)
+
+HSPs
+
+# Let's create three lists and combine into a data frame
+names <- c("Frank", "Bisoye", "Farouk", "Pascal", "Uche", "Yomi")
+biotechnology <- c(86, 70, 89, 91, 97, 72)
+pharmacology <- c(80, 94, 67, 93, 98, 76)
+
+Results <- data.frame(names, biotechnology, pharmacology)
+
+print(Results)
+
+# TO get a specific entry from the data frame
+# In this example, I searched for what Uche got in pharmacology.
+# That is, I searched for the Pharmacology score referencing name "Uche" in the Results Data frame
+Results$pharmacology[Results$names == "Uche"]
+
+Results$biotechnology[Results$names == "Bisoye"]
+
+
+
+
+
