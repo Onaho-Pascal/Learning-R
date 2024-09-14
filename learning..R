c44ed99ff5cbba0a25a4ab0d386463f752737ebb
Pascal <Pascalonaho@gmail.com>
2024-09-14 04:20:21 +0100

    updated R

---
+++ learning..R
@@ -0,0 +1,23 @@
+tinytex::install_tinytex()
+
+a <- 3
+b <- "gene"
+c <- FALSE
+print(a + c)
+print(1 + TRUE)
+
+pharmacology_scoreline <- c("uche"=89, "chimsom"=74, "pascal"=86, "fafa"=80, "oge"=81)
+pharmacology_scoreline
+?(relevel)
+?(class)
+test <- factor(c('a','C','d','b',1,'!'))
+levels(test)
+
+score = 50
+print(score)
+ls()
+x = 1:20
+x
+#attr(x, "game") = 11
+#print(x)
+methods("mean")
