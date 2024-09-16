cd2648d299d65650290c8c667f917b2bb64f6d59
Pascal <Pascalonaho@gmail.com>
2024-09-16 01:19:46 +0100

    updated

--- Task2.R
+++ Task2.R
@@ -22,6 +22,29 @@ hist(glio_data[, "TCGA.19.4065.02A.11R.2005.01"],
      col = "lightgreen", 
      breaks = 50)
 
+
+
+# Normalization of Data
+# using log transformation
+log_glio_data <- log2(glio_data + 1)
+boxplot(log_glio_data, xlab = "samples", ylab = "counts", las = 2, col = "lightblue") # las = 2 is to rotate the x-axis labels 
+hist(log_glio_data[, "TCGA.19.4065.02A.11R.2005.01"], 
+     main = "distribution of Raw Counts for Sample 1", 
+     xlab = "counts",
+     col = "lightgreen", 
+     breaks = 50)
+#using Zeta transformation
+zscoreglio_data <- t(scale(t(log_glio_data)))
+boxplot(zscoreglio_data, xlab = "samples", ylab = "counts", las = 2, col = "yellow")
+boxplot(glio_data, xlab = "samples", ylab = "counts", las = 2, col = "lightblue") # las = 2 is to rotate the x-axis labels 
+hist(zscoreglio_data[, "TCGA.19.4065.02A.11R.2005.01"], 
+     main = "distribution of Raw Counts for Sample 1", 
+     xlab = "counts", 
+     col = "lightgreen", 
+     breaks = 50)
+
+# Heatmap with Sequential Palette
+# Original data
 heatmap.2(as.matrix(glio_data),
           col = seq_color_palette,
           Rowv = F, Colv = F, dendrogram = "none",
@@ -31,5 +54,171 @@ heatmap.2(as.matrix(glio_data),
           key.title = "Expression",
           density.info = "none",
           main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
-          cexRow = 0.9, cexCol = 0.7, margins = c(10,10))
-View(glio_data)
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+# log data
+heatmap.2(as.matrix(log_glio_data),
+          col = seq_color_palette,
+          Rowv = F, Colv = F, dendrogram = "none",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+# Zeta score data
+heatmap.2(as.matrix(zscoreglio_data),
+          col = seq_color_palette,
+          Rowv = F, Colv = F, dendrogram = "none",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+
+# Heatmap with diverging palette
+# Original Data
+heatmap.2(as.matrix(glio_data),
+          col = div_color_palette,
+          Rowv = F, Colv = F, dendrogram = "none",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+
+# Log data
+heatmap.2(as.matrix(log_glio_data),
+          col = div_color_palette,
+          Rowv = F, Colv = F, dendrogram = "none",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+# Zeta score data
+heatmap.2(as.matrix(zscoreglio_data),
+          col = div_color_palette,
+          Rowv = F, Colv = F, dendrogram = "none",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+
+# Heat Map clustering by Genes
+# Original Data
+heatmap.2(as.matrix(glio_data),
+          col = div_color_palette,
+          Rowv = T, Colv = F, dendrogram = "row",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+# Log Data
+heatmap.2(as.matrix(log_glio_data),
+          col = div_color_palette,
+          Rowv = T, Colv = F, dendrogram = "row",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+# Zeta Score Data
+heatmap.2(as.matrix(zscoreglio_data),
+          col = div_color_palette,
+          Rowv = T, Colv = F, dendrogram = "row",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+
+# Heatmap clustering by sample
+# Original Data
+heatmap.2(as.matrix(glio_data),
+          col = div_color_palette,
+          Rowv = F, Colv = T, dendrogram = "column",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+
+# Log Data
+heatmap.2(as.matrix(log_glio_data),
+          col = div_color_palette,
+          Rowv = F, Colv = T, dendrogram = "column",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+
+# Zeta Score Data
+heatmap.2(as.matrix(zscoreglio_data),
+          col = div_color_palette,
+          Rowv = F, Colv = T, dendrogram = "column",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+
+# Heatmap clustering by both samples and gene
+# Original Data
+heatmap.2(as.matrix(glio_data),
+          col = div_color_palette,
+          Rowv = T, Colv = T, dendrogram = "both",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+
+# Log Data
+heatmap.2(as.matrix(log_glio_data),
+          col = div_color_palette,
+          Rowv = T, Colv = T, dendrogram = "both",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
+
+# Zeta score data
+heatmap.2(as.matrix(zscoreglio_data),
+          col = div_color_palette,
+          Rowv = T, Colv = T, dendrogram = "both",
+          sepcolor = "black",
+          trace = "none",
+          key = T,
+          key.title = "Expression",
+          density.info = "none",
+          main = "Heatmap of top 500+ differentially expressed genes in Glioblastoma",
+          cexRow = 0.9, cexCol = 0.7, margins = c(11,10))
