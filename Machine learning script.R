c06c70e63db5bd3b4b710f1b77752d9eae6b83b0
Pascal <Pascalonaho@gmail.com>
2024-09-21 02:13:00 +0100

    updated

--- Machine learning script.R
+++ Machine learning script.R
@@ -20,3 +20,17 @@ meta <- readRDS("patient2LGGsubtypes.rds")
 ## Unsupervised Machine Learning
 # K-nearest neighbour
 ## How many samples do we have under each subset?
+
+table(meta$subtype)
+boxplot(lgg_data[,1:50], col = "lightblue")
+#par(oma = c(bottom, left, top, right))
+#par(oma = c(10,0,0,0)) 
+#In R, the par() function is used to set or query graphical parameters. 
+#The oma parameter in par() stands for outer margins, which controls the size of the margins outside the entire plotting region. 
+#This is useful when you want to leave extra space around your plots, for example, to add annotations or titles that span multiple plots.
+
+log_lggdata <- log2(lgg_data + 1)
+boxplot(log_lggdata[, 1:50], col = "lightblue")
+
+head(log_lggdata)
+head(meta)
