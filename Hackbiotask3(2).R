0b4c5a0db27db7a15e56176cc4146f57311f54f2
Pascal <Pascalonaho@gmail.com>
2024-09-24 09:07:34 +0100

    updated

--- Hackbiotask3(2).R
+++ Hackbiotask3(2).R
@@ -13,7 +13,7 @@ library(pROC)
 set.seed(42)
 
 # Load the main and meta data
-carcinoma_data <- read.csv(file = "raw expression data tcga-coad.csv", header = TRUE)
+carcinoma_data <- read.csv(file = "raw_expression_data_tcga-coad.csv", header = TRUE)
 carcinoma_meta <- read.csv(file = "metadata tcga-coad0.csv", header = TRUE)
 
 install.packages("dplyr")
@@ -23,7 +23,13 @@ library(tidyr)
 
 
 
+
+rownames(carcinoma_data) <- carcinoma_data$X
+carcinoma_data$X <- NULL
+
+
 # check for missing values and remove if any
+
 anyNA(carcinoma_meta)
 sum(is.na(carcinoma_meta))
 carcinoma_meta_clean <- carcinoma_meta %>% drop_na()
@@ -31,36 +37,26 @@ anyNA(carcinoma_meta_clean)
 
 
 # View data and normalize variance using log transformation
-boxplot(carcinoma_data, col = "green")
-log_carcinoma_data <- log10(carcinoma_data + 1)
-boxplot(log_carcinoma_data, col = "green")
 
 
 
-dim(log_carcinoma_data)
+boxplot(carcinoma_data, col = "lightblue")
+log_carcinoma_data <- log2(carcinoma_data + 1)
+boxplot(log_carcinoma_data, col = "lightblue")
 
-dim(carcinoma_meta_clean)
-
-colnames(carcinoma_meta_clean)
 
 # correct the column names to be in tune with that of the meta data's column names
 colnames(log_carcinoma_data) <- gsub("\\.", "-", colnames(log_carcinoma_data))
 
-View(log_carcinoma_data)
 
 # Transpose the main data
 trans_carc_data <- data.frame(t(log_carcinoma_data))
-dim(trans_carc_data)
-View(trans_data)
-
-
-# From this point, I will be using "trans_data" and "colon_meta_clean"
 
 SDs = apply(trans_carc_data, 2, sd)
 # This line calculates the standard deviation for each column (each gene) in trans_data and stores the results in the vector SDs. 
 # A higher standard deviation indicates greater variability in the gene expression levels.
 
-topPredicts = order(SDs, decreasing = T)[1:2000]
+topPredicts = order(SDs, decreasing = T)[1:3000]
 #After calculating the standard deviation, this lines ensures that the top 2000 genes with the highest SD are selected, and in descending order.
 
 
@@ -68,34 +64,28 @@ trans_carc_data = trans_carc_data[, topPredicts]
 #The trans_data is now reduced to only include the top 2000 genes that have the highest variability, 
 # which are often more informative for subsequent analyses like classification
 
-rownames(trans_carc_data)
-
-
 rownames(carcinoma_meta_clean) <- carcinoma_meta_clean$barcode #changed the rownames from numerical "1, 2, 3, 4, 5...." to the Barcode
 
-rownames(trans_carc_data)
-rownames(carcinoma_meta_clean)
-View(carcinoma_meta_clean)
-
 carcinoma_meta_clean$barcode <- NULL #to remove the row name duplicate
-View(carcinoma_meta_clean)
+
+
+# Merge both data
 carcinoma_merged_data <- merge(trans_carc_data, carcinoma_meta_clean, by = "row.names") #merging of both main and meta data
-dim(carcinoma_merged_data)
-View(carcinoma_merged_data)
+
+
 
 # To remove near zero variation
 
 all_zero <- preProcess(carcinoma_merged_data, method = "nzv", uniqueCut = 15)
 carcinoma_merged_data <- predict(all_zero, carcinoma_merged_data)
 
-dim(carinoma_merged_data)
 
 # center. Centering helps to stabilize numerical computations 
 #and can be particularly important for algorithms sensitive to the scale of the data (like PCA, KNN, etc.).
 all_center <- preProcess(carcinoma_merged_data, method = "center")
 carcinoma_merged_data <- predict(all_center, carcinoma_merged_data)
 
-dim(carcinoma_merged_data)
+
 
 # to remove highly correlated values.
 #Reduce Multicollinearity: High correlations between features can lead to issues in model training, 
@@ -108,7 +98,6 @@ all_corr <-preProcess(carcinoma_merged_data, method = "corr", cutoff = 0.5)
 carcinoma_merged_data <- predict(all_corr, carcinoma_merged_data)
 
 
-dim(carcinoma_merged_data)
 
 
 # Splitting into training and Data Sets
@@ -118,11 +107,6 @@ carcinomatrain <- createDataPartition(y = carcinoma_merged_data$gender, p = 0.7)
 train_carcinoma <- carcinoma_merged_data[carcinomatrain,]
 test_carcinoma <- carcinoma_merged_data[-carcinomatrain,]
 
-dim(train_carcinoma)
-dim(test_carcinoma)
-View(train_carcinoma)
-View(test_carcinoma)
-
 if ("Row.names" %in% colnames(train_carcinoma)) {
   train_carcinoma$Row.names <- NULL
 }
@@ -131,30 +115,22 @@ if ("Row.names" %in% colnames(test_carcinoma)) {
   test_carcinoma$Row.names <- NULL
 }
 
-#for (column_name in names(test_colon)) {
-#if (is.factor(test_colon[[column_name]])) {
-# levels(test_colon[[column_name]]) <- levels(train_colon[[column_name]])
-#}
-#}
+
 
 # Perform the training
 
-ctrl_carcinoma <- trainControl(method = "cv", number = 5)
+ctrl_carcinoma <- trainControl(method = "cv", number =5)
 
 
 knn_carcinoma <- train(gender~., #the levels of classification
                    data = train_carcinoma, #the training dataset
                    method = "knn", # the knn method
                    trControl = ctrl_carcinoma, #the training control
-                   tuneGrid = data.frame(k = 1:10))
+                   tuneGrid = data.frame(k = 1:5))
 
 #the best K is:
 knn_carcinoma$bestTune
-colnames(train_carcinoma)
-colnames(test_carcinoma)
 
-str(train_carcinoma)
-str(test_carcinoma)
 #predict
 trainPred_carcinoma <- predict(knn_carcinoma, newdata = train_carcinoma)
 testPred_carcinoma <- predict(knn_carcinoma, newdata = test_carcinoma)
@@ -166,9 +142,28 @@ test_carcinoma$gender <- as.factor(test_carcinoma$gender)
 
 levels(trainPred_carcinoma) <- levels(train_carcinoma$gender)
 levels(testPred_carcinoma) <- levels(test_carcinoma$gender)
+
 # Interpretation
 # confusion matrix
 confusionMatrix(trainPred_carcinoma, train_carcinoma$gender)
 confusionMatrix(testPred_carcinoma, test_carcinoma$gender)
-View(knn_carcinoma)
 
+
+
+#determine the variable importance
+
+explainer_carcinoma <- explain(knn_carcinoma, 
+                               label = "knn",
+                               data = train_carcinoma,
+                               y = as.numeric(train_carcinoma$gender))
+
+# fitted_model <- knn_carcinoma$finalModel
+
+# Now use explain on the fitted model
+#explainer_carcinoma <- explain(fitted_model, 
+                               #label = "knn",
+                               #data = train_carcinoma,
+                               #y = as.numeric(train_carcinoma$gender))
+
+importance_carcinoma <- feature_importance(explainer_carcinoma, n_sample = 40, type = "difference")
+                              
--- Machine learning script.R
+++ Machine learning script.R
@@ -17,11 +17,26 @@ set.seed(34567)
 lgg_data <- readRDS("LGGrnaseq.rds")
 meta <- readRDS("patient2LGGsubtypes.rds")
 
+
+View(lgg_data)
+View(meta)
+
+dim(lgg_data)
+dim(meta)
 ## Unsupervised Machine Learning
 # K-nearest neighbour
 ## How many samples do we have under each subset?
-
+print(lgg_data)
+dim(lgg_data)
+rownames(lgg_data)
+colnames(lgg_data)
+View(lgg_data)
+dim(meta)
+print(meta)
+colnames(meta)
+anyNA(meta)
 table(meta$subtype)
+View(meta)
 boxplot(lgg_data[,1:50], col = "lightblue")
 #par(oma = c(bottom, left, top, right))
 #par(oma = c(10,0,0,0)) 
@@ -29,8 +44,63 @@ boxplot(lgg_data[,1:50], col = "lightblue")
 #The oma parameter in par() stands for outer margins, which controls the size of the margins outside the entire plotting region. 
 #This is useful when you want to leave extra space around your plots, for example, to add annotations or titles that span multiple plots.
 
-log_lggdata <- log2(lgg_data + 1)
+log_lggdata <- log10(lgg_data + 1)
 boxplot(log_lggdata[, 1:50], col = "lightblue")
 
 head(log_lggdata)
 head(meta)
+
+is.data.frame(log_lggdata)
+all_trans <- data.frame(t(log_lggdata))
+is.data.frame(all_trans)
+
+SDs = apply(all_trans, 2, sd)
+topPreds = order(SDs, decreasing = T)[1:1000]
+all_trans = all_trans[, topPreds]
+
+colnames(all_trans)
+dim(all_trans)
+View(all_trans)
+all_trans <- merge(all_trans, meta, by = "row.names")
+dim(all_trans)
+View(all_trans)
+all_trans[1:5, 1:5]
+rownames(all_trans) <- all_trans$Row.names
+all_trans[1:5, 1:5]
+all_trans <- all_trans[,-1]
+all_trans[1:5, 1:5]
+
+# Splitting into training and Data Sets
+
+intrain <- createDataPartition(y = all_trans$subtype, p = 0.7)[[1]]
+
+train.lgg <- all_trans[intrain,]
+test.lgg <- all_trans[-intrain,]
+
+dim(train.lgg)
+dim(test.lgg)
+
+# Perform the training
+
+ctrl_lgg <- trainControl(method = "cv", number = 5)
+
+knn_lgg <- train(subtype~., #the levels of classification
+                 data = train.lgg, #the training dataset
+                 method = "knn", # the knn method
+                 trControl = ctrl_lgg, #the training control
+                 tuneGrid = data.frame(k=1:20))
+
+#the best K is:
+knn_lgg$bestTune
+
+#predict
+trainPred <- predict(knn_lgg, newdata = train.lgg)
+testPred <- predict(knn_lgg, newdata = test.lgg)
+
+# Interpretation
+# confusion matrix
+confusionMatrix(trainPred, train.lgg$subtype)
+confusionMatrix(testPred, test.lgg$subtype)
+
+
+#determine variable importance
