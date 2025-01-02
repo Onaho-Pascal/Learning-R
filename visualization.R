1964522ddd9a89dcdf4768c8490a3db5a42880bb
Pascal <Pascalonaho@gmail.com>
2025-01-02 15:47:15 +0100

    updated

--- visualization.R
+++ visualization.R
@@ -1,7 +1,12 @@
 install.packages("gcookbook")
+install.packages("readr")
 library(ggplot2)
 library(dplyr)
 
 
-data <- read.csv("C:/Users/user/Desktop/R-files/Testexcel/study.csv",  stringsAsFactors = FALSE)
-data$Sex <- factor(data$Sex)
+data <- read.csv("C:/Users/user/Desktop/R-files/Testexcel/study.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# data$Sex <- factor(data$Sex)
+
+# Scattered plot: plot(nameofdata$valuetobeplacedinYaxis, nameofdata$valuetobeplacedinXaxis)
+plot(data$CGPA, data$Grade)
